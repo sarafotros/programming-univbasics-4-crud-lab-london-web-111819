48499109016ba0e98347ced8ee605206abb2be45
Sara Fotros <sarafotros1@yahoo.com>
2019-11-10 13:42:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array = ["I", "am", "really", "learning"]
-  array.shift
+  array = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
